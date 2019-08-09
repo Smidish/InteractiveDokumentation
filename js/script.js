c5d69378665e16bac87280bbf32b393c1464851f
Smilla Hinterreiter <s.hinterreiter@gmx.de>
2019-08-09 11:32:25 +0200

    INIT 3 lel

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-var botui = new BotUI('Finge');
+$(document).ready(function(){
+
+   $('#finge').append("<p>Welcome to Finge</p>");
+$('#btns').append("<button name='start'>Und los!</button>");
+});
